504610432d7e48f37f995738e3b79d24dd0dfc49
Laszlo Bacsi <lackac@lackac.hu>
2014-07-24 11:38:57 +0200

    Add contact script and update form

---
+++ source/contact.php
@@ -0,0 +1,121 @@
+<?
+
+class ContactForm {
+
+  protected $organization = 'IMPACT';
+  protected $senderMail   = 'info@impactnorge.no';
+  protected $infoMail     = 'info@impactnorge.no';
+
+  protected $subject = array(
+    'visitor' => 'Thank you for your question',
+    'impact'  => 'New question through the website from '
+  );
+
+  protected $returnMessageOnSuccess = 'Thank you for contacting us!';
+
+  protected $error = array();
+
+  /**
+   * POST fields
+   */
+  protected $email;
+  protected $question;
+  protected $honeypot;
+
+  public function __construct($email, $question, $honeypot) {
+    $this->email    = $email;
+    $this->question = $question;
+    $this->honeypot = $honeypot;
+  }
+
+  public function send() {
+    if ($this->isValid()) {
+      $this->sendMail($this->email, $this->subject['visitor'], $this->emailBody('visitor'));
+      $this->sendMail($this->infoMail, $this->subject['impact'].$this->email, $this->emailBody('impact'));
+
+      return $this->getSuccess();
+    }
+
+    return $this->getError();
+  }
+
+  protected function isValid() {
+    if (!!$this->honeypot) {
+      return false;
+    }
+
+    if (!$this->email || !preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/", $this->email)) {
+      $this->error['email'] = "Provide a valid email address!";
+    }
+
+    if (!$this->question || !strip_tags($this->question)) {
+      $this->error['question'] = "What was it that you wanted to ask?";
+    }
+
+    return (bool)(!count($this->error));
+  }
+
+  protected function getSuccess() {
+    return json_encode(array('ok' => $this->returnMessageOnSuccess));
+  }
+
+  protected function getError() {
+    return json_encode($this->error);
+  }
+
+  protected function emailBody($type)
+  {
+    if ($type == 'visitor') {
+      return nl2br("
+Dear Brother/Sister!
+
+Thank you for contacting us! We have received the following question from you:
+
+<i>".strip_tags($this->question)."</i>
+
+We try our best to answer your question as soon as possible.
+
+Blessings,
+IMPACT generation
+      ");
+    }
+
+    return nl2br("
+A new question has just arrived through the www.impactgeneration.org website. Reply to this email to answer it or forward it to the right person.
+
+The question was:
+
+<i>".strip_tags($this->question)."</i>
+
+The sender email address: ".$this->email."
+    ");
+  }
+
+  protected function sendMail($target, $subject, $body)
+  {
+    $replyToMail = ($this->email == $target) ? $this->infoMail : $this->email;
+
+    $header = '';
+    $header .= "MIME-Version: 1.0"."\r\n";
+    $header .= 'Content-Transfer-Encoding: 8bit'."\r\n";
+    $header .= 'Content-type: text/html; charset="utf-8"'."\r\n";
+    $header .= 'Return-Path: '.$this->senderMail."\r\n";
+    $header .= 'From: '.$this->organization.' <'.$this->senderMail.">\r\n";
+    $header .= 'Reply-To: '.$replyToMail."\r\n";
+    $header .= "X-Sender: <".$this->senderMail.">\r\n";
+    $header .= "X-Mailer: PHP"."\r\n";
+    $header .= "X-Priority: 2"."\r\n";
+    ini_set('sendmail_from', $this->senderMail);
+
+    return mail($target, $subject, $body, $header, "-oi -f ".$this->senderMail);
+  }
+
+}
+
+$contactForm = new ContactForm(
+  isset($_POST['email'])    ? $_POST['email']    : '',
+  isset($_POST['question']) ? $_POST['question'] : '',
+  isset($_POST['d_subject']) ? $_POST['d_subject'] : ''
+);
+
+echo $contactForm->send();
--- source/javascripts/components/contact.coffee
+++ source/javascripts/components/contact.coffee
@@ -1,19 +1,8 @@
-$('form.contact select[name=category]').change (e) ->
-  if $(this).val() is 'visas'
-    $('form.contact .for-visas').show()
-    allowSlideToGrow()
-  else
-    $('form.contact .for-visas').hide()
-
 $('form.contact').submit (e) ->
   e.preventDefault()
 
   $form = $(this)
   $submit = $form.find('input[type=submit]')
-
-  if $form.find('select[name=category]').val() isnt 'visas'
-    # clear visas related fields
-    $form.find('.for-visas').find('select, input, textarea').val('')
 
   data = $form.serialize()
 
@@ -27,8 +16,8 @@ $('form.contact').submit (e) ->
     $form.find('.alert-box').remove()
 
     if data.ok
-      $form.find('select[name=category]').before(successBox(data.ok))
-      $form.find('select, input[type=email], input[type=text], textarea').val('')
+      $form.find('input[type=email]').before(successBox(data.ok))
+      $form.find('input[type=email], textarea').val('')
     else
       for field, error of data
         $form.find("[name=#{field}]").after("<small class=\"error\">#{error}</small>")
@@ -48,12 +37,3 @@ successBox = (message) ->
     <a href="#" class="close">&times;</a>
   </div>
   """
-
-allowSlideToGrow = ->
-  return if allowSlideToGrow.haveBeenCalled
-  allowSlideToGrow.haveBeenCalled = true
-
-  height = $('#contact').outerHeight()
-  $('#contact').css
-    height: 'auto'
-    'min-height': "#{height}px"
